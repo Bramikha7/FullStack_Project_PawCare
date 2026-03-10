5773005ecd4efccbead7dce25c210fe0ee974832
Bramikha7 <bramikhabennyanu@gmail.com>
2026-03-10 18:46:28 +0530

    Updated

--- core/config.py
+++ core/config.py
@@ -5,5 +5,7 @@ USERNAME = os.getenv("DB_USERNAME", "postgres")
 PASSWORD = os.getenv("DB_PASSWORD", "AcademyRootPassword")
 HOSTNAME = os.getenv("DB_HOSTNAME", "localhost")
 PORT = os.getenv("DB_PORT", "5432")
-DATABASE = os.getenv("DATABASE", os.getenv("DB_DATABASE", "neondb"))
+# Check for database name in this order: Environment 'DATABASE', then 'DB_DATABASE', then default to 'neondb'
+DATABASE = os.getenv("DATABASE") or os.getenv("DB_DATABASE") or "neondb"
+
 
