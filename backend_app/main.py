f3d09cdfa9ebe208e643981b6b8782e3b5dfa7a2
Bramikha7 <bramikhabennyanu@gmail.com>
2026-03-17 09:44:42 +0530

    Updated

--- backend_app/main.py
+++ backend_app/main.py
@@ -25,6 +25,21 @@ app.include_router(case.router)
 app.include_router(donation.router)
 
 # Simple test route
+# @app.get("/")
+# def read_root():
+#     return {"message": "Backend is running successfully"}
+
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
+import os
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+FRONTEND_DIR = os.path.join(os.path.dirname(BASE_DIR), "frontend")
+
+app.mount("/pages", StaticFiles(directory=os.path.join(FRONTEND_DIR, "pages")), name="pages")
+app.mount("/js", StaticFiles(directory=os.path.join(FRONTEND_DIR, "js")), name="js")
+app.mount("/styles", StaticFiles(directory=os.path.join(FRONTEND_DIR, "styles")), name="styles")
+
 @app.get("/")
 def read_root():
-    return {"message": "Backend is running successfully 🚀"}
+    return FileResponse(os.path.join(FRONTEND_DIR, "index.html"))
